f48f84244d2df7c5c0603934b03c88b248d3ef94
lucasloepke <wavyknight72@gmail.com>
2024-06-13 13:19:54 -0400

    update

--- movies.py
+++ movies.py
@@ -4,6 +4,16 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import os; os.chdir(os.path.dirname(__file__))
 
+class bc:
+    HEADER = '\033[95m'
+    b = '\033[94m'
+    c = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 #---------------------------------------------------------------------------
 
 df = pd.read_csv(r'tmdb_5000_movies.csv')
@@ -13,25 +23,27 @@ df['year'] = df['release_date'].str[:4] # extracting release year from date
 df['Title'] = df['original_title']; df['score'] = df['vote_average'] # cleaning headers
 
 
-print("--- Most Profitable Movies -------------------------------------------------------------------------")
+print(f"{bc.b}---{bc.ENDC} {bc.c}Most Profitable Movies {bc.b}-------------------------------------------------------------------------{bc.ENDC}")
 sdf = df[['Title', 'year', 'budget', 'revenue', 'profit', 'score']].sort_values('profit', ascending=False)
+print(sdf.head(10).to_string(index=False))
 
-print(sdf)
-print("--- Most Profitable Years --------------------------------------------------------------------------")
-print(sdf.groupby(['year']).sum().sort_values('profit', ascending=False)['profit'].head(4).apply(lambda x: '{:.1f} billion'.format(x / 1e9)).to_string(header=False))
-print("--- Summary Statistics -----------------------------------------------------------------------------")
-
+print(f"{bc.b}---{bc.ENDC} {bc.c} Summary Statistics {bc.b}-----------------------------------------------------------------------------{bc.ENDC}")
 # Calculate statistics
 mean_score = df['score'].mean()
 mean_revenue = df['revenue'].mean()
 mean_profit = df['profit'].mean()
-
 # Print fun facts
 print(f"Average score: {mean_score:.2f}", end="\t\t")
 print(f"Average revenue: ${mean_revenue/1e6:.2f}m", end="\t\t")
 print(f"Average profit: ${mean_profit/1e6:.2f}m")
 
-print("--- Harry Potter Series ----------------------------------------------------------------------------")
+print(f"{bc.b}---{bc.ENDC} {bc.c} Most Profitable Years {bc.b}--------------------------------------------------------------------------{bc.ENDC}")
+print(sdf.groupby(['year']).sum().sort_values('profit', ascending=False)['profit'].head(4).apply(lambda x: '{:.1f} billion'.format(x / 1e9)).to_string(header=False))
+
+print(f"{bc.b}---{bc.ENDC} {bc.c} Biggest Flops {bc.b}----------------------------------------------------------------------------------{bc.ENDC}")
+print(sdf.tail(5).to_string(index=False))
+
+print(f"{bc.b}---{bc.ENDC} {bc.c} Harry Potter Series {bc.b}----------------------------------------------------------------------------{bc.ENDC}")
 # For some reason the Deathly Hallows aren't included in this dataset so I will add them.
 sdf.loc[-1] = ["Harry Potter and the Deathly Hallows: Part 1", 2010, 125e6, 980e6, 980e6-125e6, 7.7]  # adding a row
 sdf.loc[-2] = ["Harry Potter and the Deathly Hallows: Part 2", 2010, 125e6, 1340e6, 1340e6-125e6, 8.1]  # adding a row
@@ -47,4 +59,4 @@ sdf['year'] = sdf['year'].apply(lambda x: '{:.0f}'.format(x))
 sdf['score'] = sdf['score'].apply(lambda x: '{:.1f}'.format(x))
 
 print(sdf[sdf['Title'].str.contains('Harry Potter')].sort_values('year').to_string(index=False))
-print("----------------------------------------------------------------------------------------------------")
+print(f"{bc.b}----------------------------------------------------------------------------------------------------{bc.ENDC}")
